da1e9a5f20de56ecfb004662096c5bd5778b3f05
samsausage <87925618+samssausages@users.noreply.github.com>
2023-10-16 16:23:33 -0500

    Update simple-snapshot-zfs.sh
    # changed timestamp to include seconds to prevent duplicate snapshot names if < 1 minute between snapshots
    # changed snapshot name to add @simplesnap_ to differentiate from other snapshots
    # Made snapshot name configurable
    # Simplified use of Timestamp Variable in script & changed to SNAPSHOTNAME
    # Added validation for SHANPSHOT_QTY & DATASETS
    # Updated to use mapfile instead of readarray for SNAPSHOTS array
    # Added Error Checking & reporting for mapfile, zfs list, zfs get, zfs snapshot, zfs destroy

--- simple-snapshot-zfs.sh
+++ simple-snapshot-zfs.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
-#v0.2
+#v0.9
 ########################simple-snapshot-zfs#######################
 ###################### User Defined Options ######################
-
 # List your ZFS datasets.  You can add/remove sets
-DATASETS=("pool/dataset1" "pool/dataset2" "pool/dataset3" "pool/dataset4" "pool/dataset5")
-# readarray -t DATASETS < <(zfs list -o name -H)  # when replacing DATASETS above, should return all pools/Datasets, but not thouroughly tested yet
+# readarray -t DATASETS < <(zfs list -o name -H)  # when replacing DATASETS above, should return all pools/Datasets, not thoroughly tested yet.  If you test this let me know!
+DATASETS=("workpool/admin" "workpool/archive")
 
 # Set Number of Snapshots to Keep
 SHANPSHOT_QTY=100
 
+# Snapshot Name Format
+SNAPSHOTNAME=$(date "+simplesnap_%Y-%m-%d-%H:%M:%S")
+
 ###### Don't change below unless you know what you're doing ######
 ##################################################################
 
-timestamp=$(date "+%Y-%m-%d-%H:%M")
-echo "Starting Snapshot ${timestamp}"
+echo "Starting Snapshot ${SNAPSHOTNAME}"
 echo "_____________________________________________________________"
+
+## Validation Steps
+# Validate SHANPSHOT_QTY
+if ! [[ $SHANPSHOT_QTY =~ ^[0-9]+$ ]]; then
+  echo "Error: SHANPSHOT_QTY must be a positive integer."
+  exit 1
+fi
+
+# Validate DATASETS
+for dataset in "${DATASETS[@]}"; do
+  if ! zfs list "$dataset" &>/dev/null; then
+    echo "Error: Dataset '$dataset' does not exist."
+    exit 1
+  fi
+done
+
+# Function to handle errors
+handle_error() {
+  local error_message="$1"
+  echo "Error: $error_message"
+  exit 1
+}
 
 # Function to create snapshot if there is changed data
 create_snapshot_if_changed() {
@@ -25,10 +48,10 @@ create_snapshot_if_changed() {
   WRITTEN=$(zfs get -H -o value written "${DATASET}")
 
   if [[ "${WRITTEN}" != "0" ]]; then
-    local TIMESTAMP
-    TIMESTAMP="$(date '+%Y-%m-%d-%H%M')"
-    zfs snapshot "${DATASET}@${TIMESTAMP}"
-    echo "Snapshot created: ${DATASET}@${TIMESTAMP}"
+    if ! zfs snapshot "${DATASET}@${SNAPSHOTNAME}"; then
+      handle_error "Failed to create snapshot for ${DATASET}"
+    fi
+    echo "Snapshot created: ${DATASET}@${SNAPSHOTNAME}"
   else
     echo "No changes detected in ${DATASET}. No snapshot created."
   fi
@@ -38,20 +61,32 @@ create_snapshot_if_changed() {
 prune_snapshots() {
   local DATASET="$1"
   local KEEP="${SHANPSHOT_QTY}"
-  
-  local SNAPSHOTS=( $(zfs list -t snapshot -o name -s creation -r "${DATASET}" | grep "^${DATASET}@") )
-  local SNAPSHOTS_COUNT=${#SNAPSHOTS[@]}
 
+  # Declare the SNAPSHOTS array
+  declare -a SNAPSHOTS
+  # Use mapfile to split the output into the SNAPSHOTS array
+  if ! mapfile -t SNAPSHOTS < <(zfs list -t snapshot -o name -s creation -r "${DATASET}" | grep "^${DATASET}@"); then
+    echo "Error: Failed to list snapshots for ${DATASET}"
+    return 1
+  fi
+
+  local SNAPSHOTS_COUNT=${#SNAPSHOTS[@]}
   echo "Total snapshots for ${DATASET}: ${SNAPSHOTS_COUNT}"
 
   local SNAPSHOTS_SPACE
-  SNAPSHOTS_SPACE=$(zfs get -H -o value usedbysnapshots "${DATASET}")
+  if ! SNAPSHOTS_SPACE=$(zfs get -H -o value usedbysnapshots "${DATASET}"); then
+    echo "Error: Failed to get space used by snapshots for ${DATASET}"
+    return 1
+  fi
   echo "Space used by snapshots for ${DATASET}: ${SNAPSHOTS_SPACE}"
 
   if [[ ${SNAPSHOTS_COUNT} -gt ${KEEP} ]]; then
     local TO_DELETE=$((SNAPSHOTS_COUNT - KEEP))
     for i in "${SNAPSHOTS[@]:0:${TO_DELETE}}"; do
-      zfs destroy "${i}"
+      if ! zfs destroy "${i}"; then
+        echo "Error: Failed to delete snapshot: ${i}"
+        return 1
+      fi
       echo "Deleted snapshot: ${i}"
       echo "_____________________________________________________________"
     done
